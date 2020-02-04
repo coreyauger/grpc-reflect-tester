5fa9ad8fabdaf43cb8abb3154e884c4bcd764d05
Corey Auger <coreyauger@gmail.com>
2020-02-04 11:27:18 -0800

    dial

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/coreyauger/grpc-reflect-tester
 
 go 1.13
+
+require (
+	github.com/fullstorydev/grpcurl v1.4.0
+	google.golang.org/grpc v1.27.0
+)
--- main.go
+++ main.go
@@ -1,8 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/fullstorydev/grpcurl"
+	"google.golang.org/grpc"
+)
+
+var (
+	defaultReplicas int32 = 1
+	sourceReflect   grpcurl.DescriptorSource
+	ccReflect       *grpc.ClientConn
+)
 
 func main() {
 	fmt.Println("Hello, world.")
-}
 
+	var portReflect int = 80
+	var host = "10.110.84.49"
+	var err error	
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	println(fmt.Sprintf("** performing DialContext at %s:%d", host, portReflect))
+
+	ccReflect, err = grpc.DialContext(ctx, fmt.Sprintf("%s:%d", host, portReflect), grpc.WithInsecure(), grpc.WithBlock(), grpc.WithTimeout(2*time.Second))
+	if err == nil {
+		println("** Got a respons")
+		println(ccReflect)
+	} else {
+		panic(err)
+	}
+
+}
